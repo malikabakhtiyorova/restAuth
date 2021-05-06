8b2674146a2817788565edb236043676f1409162
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-05-06 14:27:07 +0500

    Set-up environment

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+
+// Route Middlewares
+app.use('/api/user', authRoute)
+
+
+
+app.listen(3000, console.log(3000))
---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.post('/register', (req, res)=> {
+    res.send('register')
+})
+
+module.exports = router
